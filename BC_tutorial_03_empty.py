3f54a8a88e353d6498637481cdcda6bd513bffb5
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2022-03-29 11:39:07 +0200

    Update BC_tutorial_03_empty.py

--- BC_tutorial_03_empty.py
+++ BC_tutorial_03_empty.py
@@ -1,24 +1,28 @@
 import numpy as np
 import cv2
+from math import *
 
-# TODO capture webcam image
+cap = cv2.VideoCapture(0)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+codec = int(cap.get(cv2.CAP_PROP_CODEC_PIXEL_FORMAT))
 
+title = 'Video image'
+cv2.namedWindow(title,  cv2.WINDOW_FREERATIO)
 
-# TODO get camera image parameters from get()
+loop, frame = cap.read()
+while loop:
+    img = np.zeros(frame.shape, np.uint8)
+    smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    img[:floor(height/2), :floor(width/2)] = smaller_frame
+    img[floor(height/2):, :floor(width/2)] = cv2.flip(smaller_frame, 0)
+    img[floor(height/2):, floor(width/2):] = cv2.flip(smaller_frame, -1)
+    img[:floor(height/2), floor(width/2):] = cv2.flip(smaller_frame, 1)
+    cv2.imshow(title, img)
 
+    loop, frame = cap.read()
+    if cv2.waitKey(1) == ord('q'):
+        loop = 0
 
-# TODO create a window for the video
-
-
-# TODO start a loop
-
-# TODO (in loop) read a camera frame and check if that was successful
-
-# TODO (in loop) create four flipped tiles of the image
-
-# TODO (in loop) display the image
-
-# TODO (in loop) press q to close the window and exit the loop
-
-
-# TODO release the video capture object and window
+cap.release()
+cv2.destroyAllWindows()
